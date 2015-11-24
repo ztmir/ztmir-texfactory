30214c1e42c71151cd8ca482bdbf23c039cdb998
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2015-11-24 06:23:20 +0100

    [qnxlab] rename INFORM -> REPORT in qnxlab6 source examples

--- src/latex/papers/qnxlab/src/lab6/deadlock1.c
+++ src/latex/papers/qnxlab/src/lab6/deadlock1.c
@@ -10,7 +10,7 @@ pthread_mutex_t mutex_b = PTHREAD_MUTEX_INITIALIZER;
 
 /* Drukuje info o rezultacie operacji (sukces/blad).
  * Jesli status!=0 (blad) konczy dzialanie programu */
-int inform(int status, const char* caller, const char* operation)
+int report(int status, const char* caller, const char* operation)
 {
   if(status != 0)
     {
@@ -21,20 +21,20 @@ int inform(int status, const char* caller, const char* operation)
   return status;
 }
 
-#define INFORM(cmd) inform(cmd, __func__, #cmd)
+#define REPORT(cmd) report(cmd, __func__, #cmd)
 
 /* Watek zamyka muteksy w kolejnosci muteks_a, muteks_b */
 void* lock_forward(void* unused)
 {
   (void)unused;  /* nie uzywamy 'arg' w funkcji  */
-  INFORM(pthread_mutex_lock(&mutex_a));
+  REPORT(pthread_mutex_lock(&mutex_a));
   sleep(1);      /* dajemy czas watkowi backward na lock(mutex_b) */
-  INFORM(pthread_mutex_lock(&mutex_b));
+  REPORT(pthread_mutex_lock(&mutex_b));
 
   printf("lock_forward: zablokowalem mutex_a i mutex_b\n");
 
-  INFORM(pthread_mutex_unlock(&mutex_b));
-  INFORM(pthread_mutex_unlock(&mutex_a));
+  REPORT(pthread_mutex_unlock(&mutex_b));
+  REPORT(pthread_mutex_unlock(&mutex_a));
   return NULL;
 }
 
@@ -42,14 +42,14 @@ void* lock_forward(void* unused)
 void* lock_backward(void* unused)
 {
   (void)unused;  /* nie uzywamy 'arg' w funkcji  */
-  INFORM(pthread_mutex_lock(&mutex_b));
+  REPORT(pthread_mutex_lock(&mutex_b));
   sleep(1);      /* dajemy czas watkowi forward na lock(mutex_a) */
-  INFORM(pthread_mutex_lock(&mutex_a));
+  REPORT(pthread_mutex_lock(&mutex_a));
 
   printf("lock_backward: zablokowalem mutex_b i mutex_a\n");
 
-  INFORM(pthread_mutex_unlock(&mutex_a));
-  INFORM(pthread_mutex_unlock(&mutex_b));
+  REPORT(pthread_mutex_unlock(&mutex_a));
+  REPORT(pthread_mutex_unlock(&mutex_b));
   return NULL;
 }
 
@@ -57,12 +57,12 @@ int main(int argc, char* argv[])
 {
   pthread_t forward, backward;
 
-  INFORM(pthread_create(&backward,NULL,lock_backward,NULL));
-  INFORM(pthread_create(&forward,NULL,lock_forward,NULL));
+  REPORT(pthread_create(&backward,NULL,lock_backward,NULL));
+  REPORT(pthread_create(&forward,NULL,lock_forward,NULL));
   printf("main: utworzono watki.\n");
   printf("main: czekam na zakonczenie watkow.\n");
-  INFORM(pthread_join(backward,NULL));
-  INFORM(pthread_join(forward,NULL));
+  REPORT(pthread_join(backward,NULL));
+  REPORT(pthread_join(forward,NULL));
   printf("main: koniec programu.\n");
 
   return EXIT_SUCCESS;
